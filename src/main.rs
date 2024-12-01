8b51c5173dac473eab78efe3b874e54dce132c74
Matthieu <matthieu.jeanson@gmail.com>
2024-11-30 22:55:36 -0800

    Add the algorithm to solve a puzzle

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,45 @@
-use piece::OrientedPiece;
-
 pub mod board;
 pub mod piece;
 
 fn main() {
     let mut board = board::Board::new();
+    let mut pieces = piece::all_pieces();
 
-    let t: OrientedPiece = OrientedPiece {
-        pattern: [
-            ['t', '0', '0', '0', '0'],
-            ['t', 't', 't', '0', '0'],
-            ['t', '0', '0', '0', '0'],
-            ['0', '0', '0', '0', '0'],
-            ['0', '0', '0', '0', '0'],
-        ],
-        top_index: 0,
-    };
-    let v: OrientedPiece = OrientedPiece {
-        pattern: [
-            ['v', 'v', 'v', '0', '0'],
-            ['0', '0', 'v', '0', '0'],
-            ['0', '0', 'v', '0', '0'],
-            ['0', '0', '0', '0', '0'],
-            ['0', '0', '0', '0', '0'],
-        ],
-        top_index: 0,
-    };
+    board.set_date(4, 13, 8);
 
-    board = board.place_piece_on_top_left(&t).expect("This should work");
-    board = board.place_piece_on_top_left(&v).expect("This should work");
+    let _result = try_placing_pieces(&mut board, &mut pieces);
+    
+}
 
-
-    println!("Got a new board:");
-    println!("{}", board);
+fn try_placing_pieces<'a>(b: &'a mut board::Board, pieces: &mut Vec<char>) -> bool {
+    if b.is_full() {
+        if pieces.is_empty() {
+            println!("Found a solution:\n{}", b);
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        for piece_index in 0..pieces.len() {
+            let this_piece = pieces[piece_index];
+            let orientations = piece::get_piece(pieces[piece_index])
+                .expect("We should be able to get orientations")
+                .possible_pieces;
+            for oriented_piece in orientations {
+                if b.place_piece_on_top_left(&oriented_piece) {
+                    //println!("Now:\n {:?}", b.table);
+                    
+                    pieces.remove(piece_index);
+                    if try_placing_pieces(b, pieces) {
+                        return true;
+                    } else {
+                        pieces.insert(piece_index, this_piece);
+                        b.remove_piece(this_piece);
+                        //println!("Then:\n {:?}", b.table);
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
